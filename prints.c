d49e024c3ae0b90cd9ff977ed282dddfe88f41bf
Frida <amina.amer.9898@gmail.com>
2023-03-26 13:26:35 -0700

    Add char and string functions

---
+++ prints.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_char - function prints char
+ * @arg: argument
+ * Return: 1
+ */
+
+int print_char(va_list arg)
+{
+	char c = va_arg(arg, int);
+
+	write(1, &c, 1);
+	return (1);
+}
+
+
+/**
+ * print_string - function prints string
+ * @arg: argument
+ * Return: write
+ */
+
+int print_string(va_list arg)
+{
+	char *s = va_arg(arg, char *);
+
+	return (write(1, s, strlen(s)));
+}
